89e7fa625188d41e5f9e634eec119beefcc27d63
manu303 <manojadabala123@gmail.com>
2025-07-07 21:30:11 +0530

    best time to sell and but stock modification

--- Array level-0/maxProfit.js
+++ Array level-0/maxProfit.js
@@ -1,7 +1,7 @@
 /*https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/ */
 let prices = [2, 1, 2, 0, 1];
 function maxProfit(prices) {
-  let min = prices[i];
+  let min = prices[0];
   let maxProfit = 0;
   for (let i = 1; i < prices.length; i++) {
     if (prices[i] < min) {
