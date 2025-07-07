6d67d2b19833cf3d7218edf52f29f473237c4e7d
manu303 <manojadabala123@gmail.com>
2025-07-07 21:12:10 +0530

    best time to buy and sell stock

---
+++ Array level-0/maxProfit.js
@@ -0,0 +1,18 @@
+/*https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/ */
+let prices = [2, 1, 2, 0, 1];
+function maxProfit(prices) {
+  let min = prices[i];
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+    if (prices[i] - min > maxProfit) {
+      maxProfit = prices[i] - min;
+    }
+  }
+  return maxProfit;
+}
+
+let res = maxProfit(prices);
+console.log(res);
