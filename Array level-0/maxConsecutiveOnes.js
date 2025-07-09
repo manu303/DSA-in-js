8e0eaa13494845b6470cbd9d83c0eb17753fb9e1
manu303 <manojadabala123@gmail.com>
2025-07-09 16:34:32 +0530

    max consecutive ones in an array

---
+++ Array level-0/maxConsecutiveOnes.js
@@ -0,0 +1,23 @@
+/*https://leetcode.com/problems/max-consecutive-ones/description/ */
+let arr = [1,1,0,1,1,1];
+function findMaxConsecutiveOnes(arr)
+{
+    let currCount =0;
+    let maxCount = 0;
+    for(let i=0;i<arr.length;i++)
+    {
+        if(arr[i]===1)
+        {
+            currCount++;
+        }
+        else
+        {
+            maxCount = Math.max(currCount,maxCount);
+            currCount = 0;
+        }
+    }
+    return Math.max(currCount,maxCount);
+}
+
+let res = findMaxConsecutiveOnes(arr);
+console.log(res);
