6c72854405e7f33ab4f93a3f8b6a69f4b12f0f76
manu303 <manojadabala123@gmail.com>
2025-07-17 11:47:31 +0530

    factorial of n

---
+++ recursion level-0/factorial.js
@@ -0,0 +1,9 @@
+//calculate factorial of a number using recursion.
+function factorial(num) {
+  if (num === 0) return 1;
+  return num < 0 || num - Math.floor(num) !== 0
+    ? "Please enter a valid number"
+    : num * factorial(num - 1);
+}
+
+console.log(factorial(5));
