c6e5246a0a4b45dc6214f4c047e7d0ffbc1bc15e
manu303 <manojadabala123@gmail.com>
2025-07-25 13:52:30 +0530

    middle of the linked list

---
+++ linked list level-0/middleOfLinkedList.js
@@ -0,0 +1,13 @@
+//to find middle node of linked list
+/*https://leetcode.com/problems/middle-of-the-linked-list/description/ */
+function middleOfLinkedList(head)
+{
+    let fast = head;
+    let slow = head;
+    while(fast!==null && fast.next!==null)
+    {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
+}
