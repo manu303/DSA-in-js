3c590c620babccf4d9d4cbd99f35b7df0b27cc14
manu303 <manojadabala123@gmail.com>
2025-07-28 08:33:11 +0530

    linked list cycle-floyd algo approach

---
+++ linked list level-0/floydAlgo.js
@@ -0,0 +1,11 @@
+/*floyd algo to detect cycle in a linked list */
+function linkedListCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (slow === fast) return true;
+  }
+  return false;
+}
