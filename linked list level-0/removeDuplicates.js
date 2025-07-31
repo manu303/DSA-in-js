6f72fcc8b9970797146f44e2bbbd1f804b8b8f6e
manu303 <manojadabala123@gmail.com>
2025-07-31 22:09:39 +0530

    remove-duplicates-from-sorted-list

---
+++ linked list level-0/removeDuplicates.js
@@ -0,0 +1,17 @@
+//remove duplicates from linked list
+function removeDuplicates(head)
+{
+    let curr = head;
+    while(curr&curr.next)
+    {
+        if(curr.val === curr.next.val)
+        {
+            curr.next = curr.next.next; 
+        }
+        else
+        {
+            curr = curr.next;
+        }
+    }
+    return head;
+}
